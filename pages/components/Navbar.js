f2dea768e32f673e829c844c233908987f0dbab7
Tomaguke <marciojrgodoi@gmail.com>
2023-06-02 17:10:19 -0300

    mais coisas

---
+++ pages/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <Image
+          src="/images/pokebola.png"
+          width={30}
+          height={30}
+          alt="Pokebola"
+        />
+        <h1>PokeNext</h1>
+      </div>
+      <ul className={styles.link_itens}>
+        <li>
+          <Link href="/" legacyBehavior>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about" legacyBehavior>
+            <a>Sobre</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
